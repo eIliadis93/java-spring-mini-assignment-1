803140e19bf0adfebaf2769ffd2a17e41466cf85
e.vrailas <e.vrailas@pmmretail.com>
2022-08-04 11:48:35 +0300

    Added Restrictions age

---
+++ src/main/java/com/acmeflix/domain/program/Restrictions.java
@@ -0,0 +1,15 @@
+package com.acmeflix.domain.program;
+
+public enum Restrictions {
+    SEVEN(7), THIRTEEN(13), SIXTEEN(16), EIGHTEEN(18), ALL(1000);
+    private final Integer age;
+
+    Restrictions(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getAge() {
+        return this.age;
+    }
+
+}
