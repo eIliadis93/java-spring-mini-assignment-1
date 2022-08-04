7c03d963e5c8242e3c2c67331265efee82cee8a5
e.vrailas <e.vrailas@pmmretail.com>
2022-08-04 11:47:21 +0300

    Added DebitCard

---
+++ src/main/java/com/acmeflix/domain/account/DebitCard.java
@@ -0,0 +1,9 @@
+package com.acmeflix.domain.account;
+
+public class DebitCard {
+    private String cardNumber;
+    private Integer securityCode;
+    private String cardHolderName;
+    private String expirationDate;
+
+}
