b3bee2311fdf5e54a7249e98d3969706872c4508
e.vrailas <e.vrailas@pmmretail.com>
2022-08-05 12:19:21 +0300

    Implemented methods

--- src/main/java/com/acmeflix/service/TvShowServiceImpl.java
+++ src/main/java/com/acmeflix/service/TvShowServiceImpl.java
@@ -1,9 +1,11 @@
 package com.acmeflix.service;
 
-import com.acmeflix.domain.Genre;
-import com.acmeflix.domain.TvShow;
+import com.acmeflix.domain.*;
 import com.acmeflix.repository.BaseRepository;
 import com.acmeflix.repository.TvShowRepository;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class TvShowServiceImpl extends BaseServiceImpl<TvShow> implements TvShowService {
     private final TvShowRepository tvShowRepository;
@@ -14,11 +16,23 @@ public class TvShowServiceImpl extends BaseServiceImpl<TvShow> implements TvShow
 
     @Override
     public BaseRepository<TvShow, Long> getRepository() {
-        return null;
+        return this.tvShowRepository;
     }
 
+    public void addEpisodes(final Season season, final Episode... episode){
+        Arrays.stream(episode).forEach(episodes -> season.getEpisodes().add(episodes));
+    }
+    public void addSeasons(final TvShow tvShow, final Season... season){
+        Arrays.stream(season).forEach(seas -> tvShow.getSeasons().add(seas));
+
+    }
     @Override
     public TvShow findTvShowByTitle(String str) {
-        return null;
+        List<TvShow> tvShows = tvShowRepository.findTvShowByTitle(str);
+        if (tvShows.size() > 1)
+            logger.trace("Number of Tv's Shows found with title {} is :{}", str, tvShows.size());
+        return tvShows.get(0);
+
+
     }
 }
