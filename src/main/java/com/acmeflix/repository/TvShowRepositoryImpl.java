bae48cbfa4421d4ed62c54febdb4084ca8be3f6b
e.vrailas <e.vrailas@pmmretail.com>
2022-08-05 12:19:09 +0300

    Implemented methods

--- src/main/java/com/acmeflix/repository/TvShowRepositoryImpl.java
+++ src/main/java/com/acmeflix/repository/TvShowRepositoryImpl.java
@@ -2,23 +2,27 @@ package com.acmeflix.repository;
 
 import com.acmeflix.domain.TvShow;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class TvShowRepositoryImpl extends BaseRepositoryImpl<TvShow> implements TvShowRepository{
+
+    private final Map<Long, TvShow> data = new LinkedHashMap<>();
+    private final AtomicLong atomicLong = new AtomicLong(1);
     @Override
     public Map<Long, TvShow> getStorage() {
-        return null;
+        return this.data;
     }
 
     @Override
     public AtomicLong getSequence() {
-        return null;
+        return this.atomicLong;
     }
 
     @Override
     public List<TvShow> findTvShowByTitle(String str) {
-        return null;
+        return this.data.values().stream().filter(shows->shows.getTitle().equalsIgnoreCase(str)).toList();
     }
 }
